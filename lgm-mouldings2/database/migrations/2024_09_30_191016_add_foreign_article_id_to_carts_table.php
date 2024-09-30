38ac8ec1dd1cefb2583c6eae099cb93aabbe2560
orlando manfredini <orlando.manfre1@gmail.com>
2024-09-30 21:12:22 +0200

    ADD: carts migration with foreign artcle_id; user_id

---
+++ lgm-mouldings2/database/migrations/2024_09_30_191016_add_foreign_article_id_to_carts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('article_id');
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            //
+            $table->dropForeign(['article_id']);
+            $table->dropColumn('article_id');
+        });
+    }
+};
